b0d92e90c823b33beb1346571f8bc8ab7783ade6
skyrim <skyrim@foxmail.com>
2026-07-08 19:40:32 +0800

    初始化脚手架

---
+++ base-api/src/main/java/cn/ezios/baseapi/framework/config/SaTokenConfig.java
@@ -0,0 +1,39 @@
+package cn.ezios.baseapi.framework.config;
+
+import cn.dev33.satoken.interceptor.SaInterceptor;
+import cn.dev33.satoken.router.SaHttpMethod;
+import cn.dev33.satoken.router.SaRouter;
+import cn.dev33.satoken.stp.StpUtil;
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SaTokenConfig implements WebMvcConfigurer {
+
+    private static final List<String> EXCLUDE_PATHS = List.of(
+            "/api/auth/login",
+            "/doc.html",
+            "/webjars/**",
+            "/favicon.ico",
+            "/swagger-resources/**",
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html"
+    );
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new SaInterceptor(handle -> SaRouter.match(
+                                SaHttpMethod.GET,
+                                SaHttpMethod.POST,
+                                SaHttpMethod.PUT,
+                                SaHttpMethod.DELETE,
+                                SaHttpMethod.PATCH)
+                        .match("/api/**")
+                        .notMatch(EXCLUDE_PATHS)
+                        .check(StpUtil::checkLogin)))
+                .addPathPatterns("/**");
+    }
+}
